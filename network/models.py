1bd56103c38006b8cde336d73e15d8a355137ea1
John Thomson <johnwt29@g.eips.ca>
2020-08-29 18:50:49 -0600

    created models and database test classes

--- network/models.py
+++ network/models.py
@@ -3,4 +3,11 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField('User', related_name="followers")
+    
+
+class Post(models.Model):
+    posting_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    time = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    likes = models.ManyToManyField(User, related_name='liked_posts')
--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
-
+from .models import User, Post
 # Create your tests here.
+class Tests(TestCase):
+    def setUp(self):
+        #  Create users
+        john = User.objects.create_user(username='jthomson', email="jthomson@yahoo.com", password='pass')
+        elon = User.objects.create_user(username='elonmusk', email="elonmusk@spacex.com", password='pass')
+        homie = User.objects.create_user(username='homie', email="homie@homie.com", password='pass')
+        john.save()
+        elon.save()
+        homie.save()
+        print(john.following)
+        # Set followers 
+        john.following.add(elon, homie);
+        elon.following.add(homie);
+        homie.following.add(elon);
+
+        #Create Posts
+        p1 = Post.objects.create(posting_user=john, content='this is a post')
+        p1.likes.add(elon)
+
+        p2 = Post.objects.create(posting_user=elon, content='rockets are cool')
+        p2.likes.add(john, homie)
+
+        p3 = Post.objects.create(posting_user=john, content='elon is cool')
+        p3.likes.add(elon)
+
+    def test_get_user_posts(self):
+        '''given a user, get all posts from the user'''
+        john = User.objects.get(username='jthomson')
+        self.assertEqual(john.posts.count(), 2)
+
+    def test_get_liked_posts(self):
+        '''given a user, get their liked posts'''
+        elon = User.objects.get(username='elonmusk')
+        self.assertEqual(elon.liked_posts.count(), 2)
+    
+    def test_get_likes_on_post(self):
+        '''given a post, get users who liked the post'''
+        post = Post.objects.get(content='rockets are cool')
+        self.assertEqual(post.likes.count(), 2)
+    def test_get_followers(self):
+        '''given a user, get the users followers'''
+        john = User.objects.get(username='jthomson')
+        self.assertEqual(john.followers.count(), 0)
